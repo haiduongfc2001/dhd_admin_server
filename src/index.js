a0568d93dfd4a0b54f57d7eb374434365e63e26a
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-07 18:41:17 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const db = require('./config/db');
-const express = require('express');
-const cors = require('cors');
+const db = require("./config/db");
+const express = require("express");
+const cors = require("cors");
 
-const moment = require('moment-timezone');
+const moment = require("moment-timezone");
 
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-const ProductRoute = require('./routes/ProductRoute');
-const MovieRoute = require('./routes/MovieRoute');
-const UserRegisterRoute = require('./routes/UserRegisterRoute');
-const UserRoute = require('./routes/UserRoute');
-const AdminRoute = require('./routes/AdminRoute');
+const ProductRoute = require("./routes/ProductRoute");
+const MovieRoute = require("./routes/MovieRoute");
+const UserRegisterRoute = require("./routes/UserRegisterRoute");
+const UserRoute = require("./routes/UserRoute");
+const AdminRoute = require("./routes/AdminRoute");
 const path = require("path");
 
 // Connect to db
@@ -22,21 +22,25 @@ db.connect();
 app.use(cors());
 
 // phục vụ các tệp tin tĩnh từ thư mục tài nguyên
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", function (req, res) {
+  res.send("Xin chào Đỗ Hải Dương!");
+});
 
 // For user route
-app.use('/', ProductRoute);
-app.use('/', MovieRoute);
-app.use('/', UserRegisterRoute);
-app.use('/', UserRoute);
-app.use('/admin', AdminRoute);
+app.use("/", ProductRoute);
+app.use("/", MovieRoute);
+app.use("/", UserRegisterRoute);
+app.use("/", UserRoute);
+app.use("/admin", AdminRoute);
 
-const dataMovies = require('./config/dataMovie.json');
-app.get('/api/all-movies', async (req, res) => {
-    res.json(dataMovies);
+const dataMovies = require("./config/dataMovie.json");
+app.get("/api/all-movies", async (req, res) => {
+  res.json(dataMovies);
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  console.log(`Listening to port ${port}`);
 });
