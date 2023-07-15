b48236523de9f58f343b809a8b96bce5942e7fa7
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-15 21:55:03 +0700

    Update index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.CONNECT_MONGODB_LOCAL, {
+    await mongoose.connect(process.env.CONNECT_MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
