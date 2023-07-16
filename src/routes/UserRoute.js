e4d1821ab2a5fc4890ee079b32cfd6d6d417063e
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-07-16 18:38:50 +0700

    upload image

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -31,11 +31,25 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, "../public/userImages"));
   },
   filename: function (req, file, cb) {
-    const name = Date.now() + "-" + file.originalname;
-    cb(null, name);
+    cb(null, new Date().toISOString() + file.originalname);
   },
 });
-const upload = multer({ storage: storage });
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5MB file size limit
+  },
+  fileFilter: fileFilter,
+});
 
 // axios
 user_route.get(
